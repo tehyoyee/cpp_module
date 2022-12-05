9808d7a8b8bb9d2c4302bd02f41aa3eec26b40ca
taehykim <tehyoyee@gmail.com>
2022-12-05 20:49:37 +0900

    add cpp_module01/ex03

---
+++ cpp_module_01/ex03/Weapon.hpp
@@ -0,0 +1,19 @@
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+
+#include <iostream>
+
+class Weapon
+{
+	public:
+		const std::string	&getType() const;
+		void				setType(std::string type);
+
+		Weapon(std::string type);
+		~Weapon();
+
+	private:
+		std::string type;
+};
+
+#endif
---
+++ cpp_module_01/ex04/main__.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+
+int	main(int ac, char *av[])
+{
+	std::ifstream ifs;
+	std::ofstream ofs;
+	std::string outfile;
+	std::string contents;
+	std::string s1;
+	std::string s2;
+	int s1_len;
+	int s2_len;
+
+	if (ac != 4)
+	{
+		std::cout << "argc is not 4" << std::endl; 
+		return (1);
+	}
+	s1 = av[2];
+	s2 = av[3];
+	s1_len = s1.length();
+	s2_len = s2.length();
+	if (std::strlen(av[1]) == 0 || s1_len == 0 || s2_len == 0)
+	{
+		std::cout << "argv length is 0" << std::endl; 
+		return (1);
+	}
+
+	ifs.open(av[1]);
+	if (ifs.fail())
+	{
+		std::cout << "sorry, can't open " << av[1] << std::endl;
+		return (1); 
+	}
+
+	outfile = av[1];
+	outfile.append(".replace"); 
+	ofs.open(outfile);
+	if (ofs.fail())
+	{
+		std::cout << "sorry, can't open " << outfile << std::endl;
+		return (1); 
+	}
+
+	while (true)
+	{
+		std::getline(ifs, contents);
+		
+		size_t pos = 0;
+		while (true)
+		{
+			pos = contents.find(s1, pos);
+			if (pos == std::string::npos)
+			{
+				break ;
+			}
+			contents.erase(pos, s1_len);
+			contents.insert(pos, s2);
+			pos += s2_len;
+		}
+		ofs << contents;
+		if (ifs.eof())
+			break ;
+		ofs << std::endl;	
+	}
+	
+	return (0);
+}
