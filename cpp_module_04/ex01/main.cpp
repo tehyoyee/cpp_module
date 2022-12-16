7ed145a8537deb8037e8caa3b28ed05118686976
taehykim <tehyoyee@gmail.com>
2022-12-16 18:47:04 +0900

    complete cppmodule04

--- cpp_module_04/ex01/main.cpp
+++ cpp_module_04/ex01/main.cpp
@@ -4,38 +4,38 @@
 
 int main()
 {
-	Animal *meta[4];
+	// Animal *meta[4];
 
-	for (int i = 0; i < 4; i++)
-	{
-		if (i % 2)
-			meta[i] = new Cat();
-		else
-			meta[i] = new Dog();
-	}
+	// for (int i = 0; i < 4; i++)
+	// {
+	// 	if (i % 2)
+	// 		meta[i] = new Cat();
+	// 	else
+	// 		meta[i] = new Dog();
+	// }
 	
-	std::cout << std::endl;
-	for (int i = 0; i < 4; i++)
-		delete meta[i];
+	// std::cout << std::endl;
+	// for (int i = 0; i < 4; i++)
+	// 	delete meta[i];
 
 /////// operation overload //////////
 
-	// Dog *d1 = new Dog();
-	// Dog *d2 = new Dog();
+	Dog *d1 = new Dog();
+	Dog *d2 = new Dog();
 
-	// *d2 = *d1;
+	*d2 = *d1;
 
-	// std::cout << std::endl;
-	// std::cout << d1->getBrain()->getIdeas(0) << std::endl;
-	// std::cout << d2->getBrain()->getIdeas(0) << std::endl;
-	// std::cout << std::endl;
-	// d1->getBrain()->setIdeas(0, "asdfasdfasdf");
-	// std::cout << d1->getBrain()->getIdeas(0) << std::endl;
-	// std::cout << d2->getBrain()->getIdeas(0) << std::endl;
-	// std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << d1->getBrain()->getIdeas(0) << std::endl;
+	std::cout << d2->getBrain()->getIdeas(0) << std::endl;
+	std::cout << std::endl;
+	d1->getBrain()->setIdeas(0, "asdfasdfasdf");
+	std::cout << d1->getBrain()->getIdeas(0) << std::endl;
+	std::cout << d2->getBrain()->getIdeas(0) << std::endl;
+	std::cout << std::endl;
 
-	// delete d1;
-	// delete d2;
+	delete d1;
+	delete d2;
 
 ///////// Copy Assignment ///////////
 
