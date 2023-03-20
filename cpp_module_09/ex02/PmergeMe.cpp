7b1a4ccf21847f1e8b0db7d67b07e63640208ba8
taehykim <tehyoyee@gmail.com>
2023-03-20 20:58:09 +0900

    add cppmodule09/ex02/making merge-insertionSort by List

--- cpp_module_09/ex02/PmergeMe.cpp
+++ cpp_module_09/ex02/PmergeMe.cpp
@@ -1,6 +1,6 @@
 #include "PmergeMe.hpp"
 
-std::vector<int>	insertionVector(std::vector<int> arr) {
+std::vector<int>	insertionSortVector(std::vector<int> arr) {
 	int tmp;
 
 	for (int i = 1; i < arr.size(); i++) {
@@ -17,7 +17,7 @@ std::vector<int>	insertionVector(std::vector<int> arr) {
 
 std::vector<int>	mergeSortVector(std::vector<int> arr, int start, int end) {
 	if (end - start < 5)
-		return insertionVector(arr);
+		return insertionSortVector(arr);
 
 	int	mid = (end + start) / 2;	
 	std::vector<int>	arrLeft;
@@ -43,5 +43,67 @@ std::vector<int>	mergeSortVector(std::vector<int> arr, int start, int end) {
 		mergedArr.push_back(arrLeft[l]);
 	for (; r < arrRight.size(); r++)
 		mergedArr.push_back(arrRight[r]);
+	return mergedArr;
+}
+
+std::list<int>	insertionSortList(std::list<int> arr) {
+	int tmp;
+
+	for (std::list<int>::iter == arr.begin(); iter != arr.end(); iter++) {
+		for (std::list<int>::iter2 == iter; iter2 != arr.begin(); iter2--) {
+			
+		}
+	}
+	// for (int i = 1; i < arr.size(); i++) {
+	// 	for (int j = i; j > 0; j--) {
+	// 		if (arr[j - 1] > arr[j]) {
+	// 			tmp = arr[j];
+	// 			arr[j] = arr[j - 1];
+	// 			arr[j - 1] = tmp;
+	// 		}
+	// 	}
+	// }
+	return arr;
+}
+
+std::list<int>	mergeSortList(std::list<int> arr, int start, int end) {
+	if (end - start < 5)
+		return insertionSortList(arr);
+	int i = 0;
+	int	mid = (end + start) / 2;	
+	std::list<int>	arrLeft;
+	std::list<int>	arrRight;
+
+	while (arrLeft.size() < mid) {
+		arrLeft.push_back(arr.front());
+		arr.pop_front();
+	}
+	while (arrRight.size < end - mid) {
+		arrRight.push_back(arr.front());
+		arr.pop_front();
+	}
+	arrLeft = mergeSortList(arrLeft, 0, arrLeft.size());
+	arrRight = mergeSortList(arrRight, 0, arrRight.size());
+
+	std::list<int>	mergedArr;
+
+	while (arrLeft.size() && arrRight.size()) {
+		if (arrLeft.front() < arrRight.front()) {
+			mergedArr.push_back(arrLeft.front());
+			arrLeft.pop_front();
+		}
+		else {
+			mergedArr.push_back(arrRight.front());
+			arrRight.pop_front();
+		}
+	}
+	while (arrLeft.size()) {
+		mergedArr.push_back(arrLeft.front());
+		arrLeft.pop_front();
+	}
+	while (arrRight.size()) {
+		mergedArr.push_back(arrRight.front());
+		arrRight.pop_front();
+	}
 	return mergedArr;
 }
--- cpp_module_09/ex02/PmergeMe.hpp
+++ cpp_module_09/ex02/PmergeMe.hpp
@@ -10,6 +10,8 @@
 #include <cctype>
 
 std::vector<int>	mergeSortVector(std::vector<int> arr, int start, int end);
-std::vector<int>	insertionVector(std::vector<int> arr);
+std::vector<int>	insertionSortVector(std::vector<int> arr);
+std::list<int>		mergeSortList(std::list<int> arr, int start, int end);
+std::list<int>		insertionSortList(std::list<int> arr);
 
 #endif
